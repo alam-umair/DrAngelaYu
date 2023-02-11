3d7879352c03d61276e06a56cf855687d393ebf7
Umair Alam <101912508+alam-umair@users.noreply.github.com>
2023-02-11 13:42:57 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import "./styles.css";
+import React from "react";
+import ReactDOM from "react-dom";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>My Favourite Food</h1>
+      <ul>
+        <li>Biryani With Raita</li>
+        <li>Chicken & Mutton Korma</li>
+        <li>Nalla Nihari</li>
+        <li>Chicken & Mutton Kabab</li>
+        <li>Butter Chicken Masala</li>
+        <li>Pizza With Coca Cola</li>
+      </ul>
+    </div>
+  );
+}
